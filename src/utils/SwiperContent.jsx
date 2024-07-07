b2f1d0d4f86d57cd989f41dfe944ccdd7bf5d1ba
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-07-07 16:29:06 +0530

    updated Movix_ app

--- src/utils/SwiperContent.jsx
+++ src/utils/SwiperContent.jsx
@@ -23,32 +23,6 @@ const SwiperContent = ()=> {
           modules={[Navigation,Scrollbar, Pagination, Keyboard]}
           className="mySwiper"
         >
-          <SwiperSlide  >
-         <Flex direction='column' gap={8} flexWrap='wrap' color='white' alignItems='center' py={16}>
-
-          <Image src='https://i.postimg.cc/Y0wTR6k1/Document-Google-Chrome-18-03-2024-00-27-55.png' w={{base:'90%',sm:'75%',md:'50%'}}/>
-
-          <Flex direction='column' fontSize={{base:16,sm:18,md:22}} fontWeight='semibold' gap={5} flexWrap='wrap' textAlign='center' color='#178582'>
-            <Text fontSize={{base:20,sm:24,md:28}} color='seagreen' fontWeight='bold'>Tata-1mg</Text>
-            <Text >It is a clone of popular healthcare platform Tata-1Mg, owned by Tata, built by using MERN stack .
-                     It is a single page application incorporating firebase authentication, authorization, browsing products, search functionality, adding to cart functionality and
-                     integrating payment gateway powered by Razorpay. It is highly responsive and has rich user interface. The API requests and CRUD operations are handled using express in
-                      node.js environment with MongoDB used as database to store data. The frontend is deployed on netlify and backend on render.  
-            </Text>
-            <Text>Tech Stack: <span style={{color:'seagreen', fontSize:'24px'}}>React | Vite | MongoDB | Express | Node.js | RESTful API | CRUD | Firebase | Chakra UI | Payment Gateway</span></Text>
-          </Flex>
-
-          <Flex justifyContent={{base:'center',sm:'space-between'}} gap={2} w={{base:'80%',sm:'80%',md:'80%'}} flexWrap='wrap'>
-            <Link href='https://github.com/Arish089/CW-2/' isExternal={true} target='_blank'>
-            <Button bgColor='#178582'>Git Hub <ExternalLinkIcon mx='2px' /></Button>
-            </Link>
-            <Link href='https://tata-1mgs.netlify.app/' target='_blank' isExternal={true}>
-            <Button bgColor='#178582'>Live Site <ExternalLinkIcon mx='2px' /></Button>
-            </Link>
-            </Flex>
-
-         </Flex>
-          </SwiperSlide>
 
           <SwiperSlide  >
          <Flex direction='column' gap={8} flexWrap='wrap' color='white' alignItems='center' py={16}>
@@ -70,6 +44,32 @@ const SwiperContent = ()=> {
             <Button bgColor='#178582'>Git Hub <ExternalLinkIcon mx='2px' /></Button>
             </Link>
             <Link href='https://movix-089.netlify.app/' target='_blank' isExternal={true}>
+            <Button bgColor='#178582'>Live Site <ExternalLinkIcon mx='2px' /></Button>
+            </Link>
+            </Flex>
+
+         </Flex>
+          </SwiperSlide>
+          <SwiperSlide  >
+         <Flex direction='column' gap={8} flexWrap='wrap' color='white' alignItems='center' py={16}>
+
+          <Image src='https://i.postimg.cc/Y0wTR6k1/Document-Google-Chrome-18-03-2024-00-27-55.png' w={{base:'90%',sm:'75%',md:'50%'}}/>
+
+          <Flex direction='column' fontSize={{base:16,sm:18,md:22}} fontWeight='semibold' gap={5} flexWrap='wrap' textAlign='center' color='#178582'>
+            <Text fontSize={{base:20,sm:24,md:28}} color='seagreen' fontWeight='bold'>Tata-1mg</Text>
+            <Text >It is a clone of popular healthcare platform Tata-1Mg, owned by Tata, built by using MERN stack .
+                     It is a single page application incorporating firebase authentication, authorization, browsing products, search functionality, adding to cart functionality and
+                     integrating payment gateway powered by Razorpay. It is highly responsive and has rich user interface. The API requests and CRUD operations are handled using express in
+                      node.js environment with MongoDB used as database to store data. The frontend is deployed on netlify and backend on render.  
+            </Text>
+            <Text>Tech Stack: <span style={{color:'seagreen', fontSize:'24px'}}>React | Vite | MongoDB | Express | Node.js | RESTful API | CRUD | Firebase | Chakra UI | Payment Gateway</span></Text>
+          </Flex>
+
+          <Flex justifyContent={{base:'center',sm:'space-between'}} gap={2} w={{base:'80%',sm:'80%',md:'80%'}} flexWrap='wrap'>
+            <Link href='https://github.com/Arish089/CW-2/' isExternal={true} target='_blank'>
+            <Button bgColor='#178582'>Git Hub <ExternalLinkIcon mx='2px' /></Button>
+            </Link>
+            <Link href='https://tata-1mgs.netlify.app/' target='_blank' isExternal={true}>
             <Button bgColor='#178582'>Live Site <ExternalLinkIcon mx='2px' /></Button>
             </Link>
             </Flex>
