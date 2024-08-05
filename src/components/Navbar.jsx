827429a2d5a4a0bead39989ea769b4a880c810f3
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-08-05 17:18:43 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
        <SmoothScrollLink to='#contact'>  <Text className='NavCon'>Contact</Text></SmoothScrollLink>
     </Flex>
     <Box>
-      <Link href='https://drive.google.com/file/d/147rPt6nS3Kivv3RTDbC9XcEeu_9-zzJc/view?usp=sharing' download='ArishAli-FullStackWebDeveloper-mQU7.pdf'>
+      <Link href='https://drive.google.com/file/d/1vNoscNnEkizqMrIrDt4IBJt92VohefJc/view?usp=sharing' download='ArishAli-FullStackWebDeveloper-XxjR.pdf'>
       <Button bgColor='#178582' color='#0A1828'  fontSize={24} fontWeight='bold'>Resume</Button>
       </Link>
     </Box>
@@ -55,7 +55,7 @@ const Navbar = () => {
               <Text className='NavCon1'>Contact</Text>
             </Flex>
             <Box>
-            <Link href='https://drive.google.com/file/d/147rPt6nS3Kivv3RTDbC9XcEeu_9-zzJc/view?usp=sharing' download='ArishAli-FullStackWebDeveloper-mQU7.pdf'>
+            <Link href='https://drive.google.com/file/d/1vNoscNnEkizqMrIrDt4IBJt92VohefJc/view?usp=sharing' download='ArishAli-FullStackWebDeveloper-XxjR.pdf'>
       <Button bgColor='#178582' fontSize={24} fontWeight='bold'>Resume</Button>
       </Link>
             </Box>
